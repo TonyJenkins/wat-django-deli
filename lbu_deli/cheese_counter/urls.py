167fe4051d0fbe65d92b38114a12ecfdb830e4e3
Tony Jenkins <tony@tony-jenkins.co.uk>
2025-11-04 14:26:20 +0000

    Add full cheese list

--- lbu_deli/cheese_counter/urls.py
+++ lbu_deli/cheese_counter/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'cheese_counter'
 
 urlpatterns = [
+    path('', views.cheese_list, name='cheese_list'),
     path('<slug:slug>', views.cheese_detail, name='cheese_detail'),
 ]
--- lbu_deli/cheese_counter/views.py
+++ lbu_deli/cheese_counter/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 from .models import Cheese
 
 
+def cheese_list(request):
+    cheeses = Cheese.objects.all()
+
+    context = {
+        'cheeses': cheeses,
+    }
+
+    return render(request, 'cheese_counter/cheese_list.html', context)
+
 def cheese_detail(request, slug):
     cheese = Cheese.objects.get(slug=slug)
 
